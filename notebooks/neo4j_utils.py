30c2d902b6c7a35e97d8de0d6181c05a4b77d1a8
Peter Rose <pwrose.ucsd@gmail.com>
2022-07-23 16:35:34 -0700

    setup for Windows

--- notebooks/neo4j_utils.py
+++ notebooks/neo4j_utils.py
@@ -25,15 +25,21 @@ def unzip(filename):
     with zipfile.ZipFile(filename,"r") as zip_ref:
         zip_ref.extractall()
 
-def download_neo4j(version, password):
+def download_neo4j():
+    version = os.getenv("NEO4J_VERSION", default="neo4j-community-4.4.9")
+    password = os.getenv("NEO4J_PASSWORD", default="demo")
+    
     if not os.path.isdir(version):
         if platform.system() == "Windows":
             url = f"https://dist.neo4j.org/{version}-windows.zip"
+            neo4j_admin = os.path.join(version, "bin", "neo4j-admin.bat")
             filename = f"{version}-windows.zip"
             download_http(url, filename)
             unzip(filename)
+
         else:
             url = f"https://dist.neo4j.org/{version}-unix.tar.gz"
+            neo4j_admin = os.path.join(version, "bin", "neo4j-admin")
             filename = f"{version}-unix.tar.gz"
             download_http(url, filename)
             untar(filename)
@@ -41,29 +47,44 @@ def download_neo4j(version, password):
         if os.path.exists(filename):
             os.remove(filename)
 
-        neo4j_admin = os.path.join(version, "bin", "neo4j-admin")
         subprocess.run([neo4j_admin, "set-initial-password", password])
+        
+        if platform.system() == "Windows":
+            subprocess.run([neo4j, "install-service"])
     
 def start():
     version = os.getenv("NEO4J_VERSION", default="neo4j-community-4.4.9")
-    password = os.getenv("NEO4J_PASSWORD", default="demo")
     
     if not os.path.isdir(version):
-        download_neo4j(version, password)
-    
-    neo4j = os.path.join(version, "bin", "neo4j")
+        download_neo4j()
+        
+    if platform.system() == "Windows":
+        neo4j = os.path.join(version, "bin", "neo4j.bat")
+    else:
+        neo4j = os.path.join(version, "bin", "neo4j")
+                      
     subprocess.run([neo4j, "start"])
 
     time.sleep(30)
     
 def stop():
     version = os.getenv("NEO4J_VERSION", default="neo4j-community-4.4.9")
-    neo4j = os.path.join(version, "bin", "neo4j")
+    
+    if platform.system() == "Windows":
+        neo4j = os.path.join(version, "bin", "neo4j.bat")
+    else:
+        neo4j = os.path.join(version, "bin", "neo4j")
+                             
     subprocess.run([neo4j, "stop"]) 
     
 def status():
     version = os.getenv("NEO4J_VERSION", default="neo4j-community-4.4.9")
-    neo4j = os.path.join(version, "bin", "neo4j")
+                        
+    if platform.system() == "Windows":
+        neo4j = os.path.join(version, "bin", "neo4j.bat")
+    else:
+        neo4j = os.path.join(version, "bin", "neo4j") 
+        
     status = subprocess.run([neo4j, "status"])
     print(status)
     
